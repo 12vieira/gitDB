9b341eaac370fdc064af72943203aa7d37826215
MATHEUS PIMENTEL VIEIRA <a92263706@rn.senac.local>
2025-05-06 21:11:41 -0300

    aulas mdv

---
+++ AULA/src/modules/aluno/views/index.js
@@ -0,0 +1,68 @@
+import AlunoController from "../controllers/index.js";
+import input from 'prompt-sync';
+
+const prompt = input();
+class AlunoView {
+  static async criar() {
+    const nome = prompt("Digite o nome do aluno: ");
+    const email = prompt("Digite o email do aluno: ");
+    const matricula = prompt("Digite a matrícula do aluno: ");
+    const telefone = prompt("Digite o telefone do aluno: ");
+    const cod_curso = prompt("Digite o código da turma do aluno: ");
+
+    const aluno = await AlunoController.criar(
+      nome,
+      email,
+      matricula,
+      telefone,
+      cod_curso
+    );
+    console.table(aluno);
+  }
+
+  static async editarAluno() {
+    const nome = prompt("Digite o novo nome do aluno: ");
+    const email = prompt("Digite o email do aluno: ");
+    const matricula = prompt("Digite a nova matrícula do aluno: ");
+    const telefone = prompt("Digite o novo telefone do aluno: ");
+    const cod_curso = prompt("Digite o novo código da turma do aluno: ");
+
+    const aluno = await AlunoController.editar(
+      nome,
+      email,
+      matricula,
+      telefone,
+      cod_curso
+    );
+    console.table(aluno);
+  }
+
+  static async listarTodos() {
+    const alunos = await AlunoController.listarTodos();
+    console.table(alunos);
+  }
+
+  static async listarPorEmail() {
+    const email = prompt("Digite o email do aluno: ");
+    const aluno = await AlunoController.listarPorEmail(email);
+    console.table(aluno);
+  }
+
+  static async deletarTodos() {
+    await AlunoController.deletarTodos();
+    console.log("Todos os alunos foram deletados.");
+  }
+
+  static async deletarAluno() {
+    const email = prompt("Digite o email do aluno a ser deletado: ");
+    await AlunoController.deletarAluno(email);
+    console.log(`Aluno com email ${email} foi deletado.`);
+  }
+
+  static async totalAlunos() {
+    const total = await AlunoController.totalAlunos();
+    console.table(total);
+  }
+}
+
+export default AlunoView;
---
+++ AULA/src/modules/professor/models/index.js
@@ -0,0 +1,42 @@
+
+// MODELS - Professor
+import client from '../../../config/database.js';
+
+class ProfessorModel {
+   static async criar(nome, matricula, cod_curso) {
+    const dados = [nome, matricula, cod_curso];
+    const consulta = `insert into professor(nome, matricula, cod_curso)
+     values ($1, $2, $3) returning *;`;
+    const resultado = await client.query(consulta, dados);
+    return resultado.rows;
+  }
+  static async listarTodos() {
+    const consulta = `select * from professor`;
+    const resultado = await client.query(consulta);
+    return resultado.rows;
+  }
+  static async atualizarProfessor(nome, matricula, cod_curso) {
+    const dados = [nome, cod_curso, matricula];
+    const consulta = `update professor set nome = $1, cod_curso = $2 where matricula = $3 returning *`;
+    const resultado = await client.query(consulta, dados);
+    return resultado.rows;
+  }
+  static async deletarProfessor(matricula) {
+    const dados = [matricula];
+    const consulta = `delete from professor where matricula = $1`;
+    await client.query(consulta, dados);
+  }
+  static async listarPorMatricula(matricula) {
+    const dados = [matricula];
+    const consulta = `select * from professor where matricula = $1`;
+    const resultado = await client.query(consulta, dados);
+    return resultado.rows;
+  }
+  static async totalProfessor(){
+      const consulta = `select count(matricula) as total from professor;`
+      const resultado = await client.query(consulta)
+      return resultado.rows
+  }
+}
+
+export default ProfessorModel;
